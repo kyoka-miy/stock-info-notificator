fcf9e4ae8b4a6517a168eebda126c40b7709fd53
宮村京花 <kyoka.miyamura21@gmail.com>
2026-01-03 14:29:44 -0800

    Fix line message pushMessage

--- src/interactor/abstractSchedulerInteractor.ts
+++ src/interactor/abstractSchedulerInteractor.ts
@@ -3,23 +3,31 @@ import { messagingApi } from "@line/bot-sdk";
 const lineClient = new messagingApi.MessagingApiClient({
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN || "",
 });
+const MAX_NUMBER_OF_MESSAGES = 5;
 
 export abstract class AbstractSchedulerInteractor {
   abstract execute(): Promise<void>;
 
   async sendLineMessages(messages: string[]): Promise<void> {
-    const lineMessages: messagingApi.Message[] = messages.map((message) => ({
-      type: "text",
-      text: message,
-    }));
-
-    try {
-      await lineClient.pushMessage({
-        to: process.env.LINE_USER_ID || "",
-        messages: lineMessages,
-      });
-    } catch (error) {
-      console.error("Failed to send notification to LINE:", error);
+    if (messages.length === 0) {
+      console.log("No messages to send.");
+      return;
+    }
+    for (let i = 0; i < messages.length; i += MAX_NUMBER_OF_MESSAGES) {
+      const chunk: messagingApi.Message[] = messages
+        .slice(i, i + MAX_NUMBER_OF_MESSAGES)
+        .map((message) => ({
+          type: "text",
+          text: message,
+        }));
+      try {
+        await lineClient.pushMessage({
+          to: process.env.LINE_USER_ID || "",
+          messages: chunk,
+        });
+      } catch (error) {
+        console.error("Failed to send notification to LINE:", error);
+      }
     }
   }
 }
